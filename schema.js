fb9aac0fd6f41263b08d4e69a0f5a84a68adf3ed
Santosh Narayanan <santosh33@gmail.com>
2023-02-15 23:43:46 +0100

    Create Schema

---
+++ schema.js
@@ -0,0 +1,11 @@
+const graphql = require('graphql');
+const {GraphQLString, GraphQLInt} = require("graphql");
+const {  GraphQLObjectType } = graphql;
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: {type:GraphQLString },
+        firstName: {type:GraphQLString} ,
+        age:{type:GraphQLInt}
+    }});
