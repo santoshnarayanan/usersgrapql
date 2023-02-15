aa4ffe1560a51ddcd7780839cf5df11ddd4ac96f
Santosh Narayanan <santosh33@gmail.com>
2023-02-15 23:56:42 +0100

    Create RootQuery

--- schema.js
+++ schema.js
@@ -9,3 +9,16 @@ const UserType = new GraphQLObjectType({
         firstName: {type:GraphQLString} ,
         age:{type:GraphQLInt}
     }});
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields: {
+        user:{
+          type: UserType,
+          args:{id: {type: GraphQLString}},
+            resolve(parentValue, args){
+
+            }
+        }
+    }
+});
